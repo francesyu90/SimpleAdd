8567828d1c0b4afce969a37431e732f5255e698d
Frances Yu <francesyu90@yahoo.com>
2018-09-21 21:12:07 -0700

    Added feature for displaying equation with randomly generated numbers

--- simpleadd/src/components/DisplayEquation.js
+++ simpleadd/src/components/DisplayEquation.js
@@ -3,20 +3,31 @@ import React, { Component } from 'react';
 class DisplayEquation extends Component {
 
     state = {
-        x: 3,
-        y: 4,
-        z: 5,
-        p: 12
+        x: Math.floor(Math.random() * 101),
+        y: Math.floor(Math.random() * 101),
+        z: Math.floor(Math.random() * 101),
+    }
+
+    computeSum = (x, y, z) => (x + y + z);
+
+    generateDisplayResult() {
+
+        if(Math.floor((Math.random() * 10) + 1) % 2 == 0) {
+            return this.computeSum(this.state.x, this.state.y, this.state.z)
+        } else {
+            return Math.floor(Math.random() * 301);
+        }
     }
 
     render() {
         return(
             <div className="displayEquation">
-                <p>{this.state.x} + {this.state.y} + {this.state.z} = {this.state.p}</p>
+                <p>
+                    {this.state.x} + {this.state.y} + {this.state.z} =  {this.generateDisplayResult()}
+                </p>
             </div>
         );
     }
-
 }
 
 export default DisplayEquation;
