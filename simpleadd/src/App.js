ba90ae92fd598066bd87769835cbf466045c906f
Frances Yu <francesyu90@yahoo.com>
2018-09-21 21:19:28 -0700

    Refactored code

--- simpleadd/src/App.js
+++ simpleadd/src/App.js
@@ -3,10 +3,28 @@ import './App.css';
 import DisplayEquation from './components/DisplayEquation';
 
 class App extends Component {
+
+  state = {
+      x: Math.floor(Math.random() * 101),
+      y: Math.floor(Math.random() * 101),
+      z: Math.floor(Math.random() * 101),
+  }
+
+  computeSum = (x, y, z) => (x + y + z);
+
+  generateDisplayResult() {
+
+      if(Math.floor((Math.random() * 10) + 1) % 2 == 0) {
+          return this.computeSum(this.state.x, this.state.y, this.state.z)
+      } else {
+          return Math.floor(Math.random() * 301);
+      }
+  }
+
   render() {
     return (
       <div className="app">
-        <DisplayEquation />
+        <DisplayEquation x={this.state.x} y={this.state.y} z={this.state.z} r={this.generateDisplayResult()} />
       </div>
     );
   }
--- simpleadd/src/components/DisplayEquation.js
+++ simpleadd/src/components/DisplayEquation.js
@@ -2,28 +2,11 @@ import React, { Component } from 'react';
 
 class DisplayEquation extends Component {
 
-    state = {
-        x: Math.floor(Math.random() * 101),
-        y: Math.floor(Math.random() * 101),
-        z: Math.floor(Math.random() * 101),
-    }
-
-    computeSum = (x, y, z) => (x + y + z);
-
-    generateDisplayResult() {
-
-        if(Math.floor((Math.random() * 10) + 1) % 2 == 0) {
-            return this.computeSum(this.state.x, this.state.y, this.state.z)
-        } else {
-            return Math.floor(Math.random() * 301);
-        }
-    }
-
     render() {
         return(
             <div className="displayEquation">
                 <p>
-                    {this.state.x} + {this.state.y} + {this.state.z} =  {this.generateDisplayResult()}
+                    {this.props.x} + {this.props.y} + {this.props.z} =  {this.props.r}
                 </p>
             </div>
         );
